0c3e615fbb106f7fa5611f0c180a5949d23880b1
Bambang <bpranoto@gmail.com>
2023-01-11 10:09:51 +0700

    wgetgdrive.sh: Download file dari google drive melalui command line

---
+++ wgetgdrive.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+if [[ -z $1 || -z $2 ]]
+then
+  echo "Syntax: wgetgdrive <IdDownload> <FileName>"
+  exit
+fi
+
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=FILEID' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=$1" -O $2 && rm -rf /tmp/cookies.txt
